39aa29406557dd850cdb075d8de893f0b403af55
purebleusong <helloworld@fuck.com>
2015-11-15 00:27:21 +0800

    some adapter

--- StoFlo.py
+++ StoFlo.py
@@ -1,6 +1,8 @@
 #coding:utf-8
 import sublime, sublime_plugin
 import engine, stools
+# 一个插件150行……我是人民的罪人
+
 helpinfo = u'''帮助信息：
 输入S保存进度
 输入L载入进度
@@ -13,6 +15,8 @@ functions = {}
 all_options_num = 0
 current_story = ''
 current_chapter = ''
+
+storys = engine.getStoryList()
 
 prefix_dict = {
 	' ': '',
@@ -36,40 +40,39 @@ def show_chapter(chapter):
 def show_tips_info(info):
 	insert_2_gamefile(info, '> ')
 
-
-
-def show_options(options, function):
+def show_options(options, function, switch=True):
 	global all_options_num
 	all_options_num = len(options + functions.keys())
-	sublime.active_window().show_quick_panel(options+functions.keys(), function)
+	sublime.active_window().show_quick_panel(options+functions.keys() if switch else options, function)
 
-def get_choose():
-	global choose
-	print 'get_choose' + str(choose)
-	return choose
 
 def save():
 	engine.save()
 	show_tips_info('Save succeed!')
+	return 0
 
 functions['save'] = save
 
+def load_E(choose):
+	global current_chapter
+	current_chapter = engine.load(choose)
+	show_chapter(engine.getChapterContent(current_chapter))
+	show_options(engine.getNextChapters(current_chapter), visit_chapter_E)
+
 def load():
+	global current_chapter
 	saves = engine.get_saves()
 	options = []
 	for save in saves:
 		options += [str(save[0])+'\t' + save[1] + '\t' + save[2]]
-	show_options(options)
-	i = get_choose()
-	chapter = engine.load(i-1)
-	show_chapter(engine.getChapterContent(chapter))
-	show_options(engine.getNextChapters(chapter))
-	return chapter
+	show_options(options, load_E, False)
+	return -1
 
 functions['load'] = load
 
 def help():
 	show_tips_info(helpinfo)
+	return 0
 
 functions['help'] = help
 
@@ -86,43 +89,49 @@ def input_handle(num):
 	print all_options_num, func_point
 	functions_num = len(functions)
 	if func_point < functions_num:
-		functions[functions.keys(functions_num - func_point + 1)]()
-		choose = 0
+		choose = functions[functions.keys(functions_num - func_point + 1)]()
 	else:
 		choose = num+1
 	print 'set_choose'+str(choose)
 	return choose
 
 def choose_story_E(num):
-	global current_story
-	num = input_handle(num) 
-	if not num:
+	global current_story, current_chapter
+	num = input_handle(num)
+	if num > 0:
 		print 'num' + str(num)
 		current_story = storys[num-1]
 		engine.chooseStory(current_story)
 		print sto + 'start'
 		visit_story(current_story)
+	elif num < 0:
+		pass
+	else:
+		show_options(storys, choose_story_E)
+
 
 def choose_chapter(chapters):
 	show_options(chapters)
 	return get_choose()
 
+def visit_chapter_E(num):
+	num = input_handle(num)
+	if num > 0:
+		show_options()
+
 def visit_chapter(chapter):
-	while(chapter is not endFlag):# loop the story line
-		show_chapter(engine.getChapterContent(chapter))
-		show_options(engine.getNextChapters(chapter))
+	show_chapter(engine.getChapterContent(chapter))
+	show_options(engine.getNextChapters(chapter))
+	choose = get_choose()
+	while not choose:
 		choose = get_choose()
-		while not choose:
-			choose = get_choose()
-		chapter = engine.chooseChapter(choose-1)
+	chapter = engine.chooseChapter(choose-1)
 	show_chapter(engine.getChapterContent(endFlag))
 
 def visit_story(story):
 	engine.resetCurrentChapter()
 	visit_chapter(startFlag)
 
-
-storys = engine.getStoryList()
 
 def startGame():
 	global storys, current_story, current_chapter
--- workRecord.md
+++ workRecord.md
@@ -1,6 +1,11 @@
 WorkPlan
 =======
 
+11.15
+------
+load适配完成，有一些问题，引擎还算好用。
+适配startchapters
+
 11.14
 ------
 因为每次showOptions以后ST都会开一个线程处理这件事，所以要适配，没个showOptions后面接一个方法，现在工作到choose story了，在触发功能功能事件以后会发生什么事情，比如load，load是应该继续载入到currentchapter
